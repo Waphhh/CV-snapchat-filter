d1cfaea82c186b339176b6ad1d672925e8b24467
Waphhh <104120471+Waphhh@users.noreply.github.com>
2024-02-15 19:07:38 +0800

    MAIN FEATURES

--- main.py
+++ main.py
@@ -7,48 +7,82 @@ import tkinter as tk
 import time
 import gui
 import os
+import sys
+
+# wierd reccursions occur that exceed the default limit
+# this just lets it occur
+sys.setrecursionlimit(1000000000)
 
 assetData = {
 
-    "head": {
-        "mask": {
+    "head": [
+        {
             "name": "mask.png",
             "scaleFactor": 1,
             "offset": 20
         },
-        "swag": {
+        {
             "name": "swag.png",
-            "scaleFactor": 1.8
+            "scaleFactor": 1.8,
+            "offset": 0
         },
-        "birthday_hat": {
-            "name": "birthday",
-            "scaleFactor": 1.5,
-            "offset": 150
-        }
-    },
+        {
+            "name": "birthday_hat.png",
+            "scaleFactor": 0.6,
+            "offset": 300
+        },
+        None
+    ],
 
-    "nose": {
-        "red_nose": {
+    "nose": [
+        {
             "name": "red_nose.png",
-            "scaleFactor": 0.4
+            "scaleFactor": 0.4,
+            "offset": 0
         },
-        "pig_nose": {
+        {
             "name": "pig_nose.png",
-            "scaleFactor": 0.15
+            "scaleFactor": 0.15,
+            "offset": 0
         },
-        "nose": {
+        {
             "name": "nose.png",
-            "scaleFactor": 0.058
-        }
-    }
+            "scaleFactor": 0.058,
+            "offset": 0
+        },
+        None
+    ]
 }
 
-root = tk.Tk()
-
-def tkinter_thread_func(root):
+def errorGUI():
     errorWindow = gui.errorButton()
-    root.destroy()
     errorWindow.mainloop()
+
+# Paths to images
+headImagesPaths = ["assets/mask.png", "assets/swag.png", "assets/birthday_hat.png", "assets/no_photo.png"]
+noseImagesPaths = ["assets/red_nose.png", "assets/pig_nose.png", "assets/nose.png", "assets/no_photo.png"]
+themesPaths = ["assets/SST_logo.png", "assets/colors.png", "assets/invert.png", "assets/no_photo.png"]
+
+rows = 2
+cols = 2
+
+root = gui.ImageGrid(headImagesPaths, rows, cols)
+root.mainloop()
+selected_head = root.return_selected_image()
+
+root = None
+
+root = gui.ImageGrid(noseImagesPaths, rows, cols)
+root.mainloop()
+selected_nose = root.return_selected_image()
+
+headData = assetData["head"][selected_head]
+noseData = assetData["nose"][selected_nose]
+
+# clear up some memory
+root = None
+
+tk.NoDefaultRoot()
 
 # Open a video capture object (camera index 0)
 cap = cv2.VideoCapture(0)
@@ -58,13 +92,6 @@ detector, predictor = ft.loadModel()
 errorCount = 0
 
 errorFlag = False
-
-# Paths to images
-headImagesPaths = ["assets/mask.png", "assets/swag.png", "assets/birthday_hat.png", "assets/no_photo.png"]
-noseImagesPaths = ["assets/red_nose.png", "assets/pig_nose.png", "assets/nose.png", "assets/no_photo.png"]
-themesPaths = ["assets/SST_logo.png", "assets/colors.png", "assets/invert.png", "assets/no_photo.png"]
-
-
 
 while True:
     faces, gray, frame = ft.videoCaptureToGrayScale(cap, detector)
@@ -76,12 +103,9 @@ while True:
             errorFlag = True
 
     if errorFlag:
-        tkinter_thread_func(root)
-        print("test")
-        root = tk.Tk()
+        errorGUI()
         errorFlag = False
         continue
-
     else:
         for face in faces:
 
@@ -89,12 +113,16 @@ while True:
 
             rotation_angle = ft.calculate_rotation_angle(tip_of_nose, middle_of_eyes)
 
-            # Getting the image to overlay
-            eyeImage = ft.loadImageToOverlay("mask.png")
-            noseImage = ft.loadImageToOverlay("red_nose.png")
+            # headImage = ft.loadImageToOverlay("swag.png")
+            # frame = ft.overlayImage(frame, headImage, middle_of_eyes[0], middle_of_eyes[1], rotation_angle, 1.8, 0)
 
-            frame = ft.overlayImage(frame, eyeImage, middle_of_eyes[0], middle_of_eyes[1], rotation_angle, 1, 20)
-            frame = ft.overlayImage(frame, noseImage, tip_of_nose[0], tip_of_nose[1], rotation_angle, 0.4)
+            if headData != None:
+                headImage = ft.loadImageToOverlay(headData["name"])
+                frame = ft.overlayImage(frame, headImage, middle_of_eyes[0], middle_of_eyes[1], rotation_angle, headData["scaleFactor"], headData["offset"])
+
+            if noseData != None:
+                noseImage = ft.loadImageToOverlay(noseData["name"])            
+                frame = ft.overlayImage(frame, noseImage, tip_of_nose[0], tip_of_nose[1], rotation_angle, noseData["scaleFactor"], noseData["offset"])
 
     # Always put the sst logo 
     frame = ft.overlaySSTLogo(frame)
